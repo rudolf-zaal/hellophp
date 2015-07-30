c9a8be9ae5c0539f06e252dff46dd65d4726cc75
rudolf-zaal <rudolf-zaal@ya.ru>
2015-07-30 14:47:13 +0300

    adding login.php

--- login.php
+++ login.php
@@ -6,6 +6,18 @@
 	</head>
 	<body>
 		<div>
+			<p>Логин: 
+				<?php
+					echo $_POST['login']
+				?>
+			</p>
+			<p>Пароль: 
+				<?php
+					echo $_POST['password']
+				?>
+			</p>
+		</div>
+		<div>
 			<form action = "login.php">
 				<p><b>Введите пару логин/пароль</b></p>
 				<p>Логин<input type="text" name="login" value="login"></p>
