e042b953ea7e2eb3feed55d26fe8590c0b9d4c9f
rudolf-zaal <rudolf-zaal@ya.ru>
2015-07-31 15:06:49 +0300

    adding login.php

--- login.php
+++ login.php
@@ -1,41 +1,8 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Васята</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	</head>
-	<body>
-		<div>
-			<p>Session ID:
-				<?php
-					echo session_id();
-					session_start();
-					echo "(".session_id().")";
-					exit();
-				?>
-			</p>
-		</div>
-<!--
-		<div>
-			<p>Логин: 
-				<?php
-					echo $_POST['login']
-				?>
-			</p>
-			<p>Пароль: 
-				<?php
-					echo $_POST['password']
-				?>
-			</p>
-		</div>
-		<div>
-			<form action = "login.php">
-				<p><b>Введите пару логин/пароль</b></p>
-				<p>Логин<input type="text" name="login" value="login"></p>
-				<p>Пароль<input type="text" name="password" value="password"></p>
-				<p><input type="submit"></p> 
-			</form>
-		</div>
-	-->
-	</body>
-</html>
+<?php
+	session_start();
+	if($_POST['login'] == "user" && $_POST['password'] == "password"){
+		$_SESSION['logged_user'] = 1;
+	}	
+	header("Location: mycms.html");
+	exit;
+?>
---
+++ mycms.php
@@ -0,0 +1,50 @@
+<?php
+	session_start();
+	unset($logged_user);
+	session_register($logged_user);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>myCms</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" type="text/css" href="css/mycms.css">
+	</head>
+	<body>
+		<div class="header">
+			<div class="headPanel">
+				myCMS
+			</div>
+			<div class="signinPanel">	
+				<?php
+					if(isset($logged_user)){
+						echo 'user';
+					}else{
+						<a href="login.html">Войти</a>
+					}
+				?>
+			</div>
+		</div>
+		<div class="content">
+			<div class="leftPanel">
+				leftPanel
+			</div>	
+			<div class="rightPanel">
+				rightPanel
+			</div>
+			<div class="mainPanel">
+				mainPanel
+			</div>
+			
+		</div>
+		<!--
+			<form action = "login.php" method="post">
+				<p><b>Введите пару логин/пароль</b></p>
+				<p>Логин<input type="text" name="login" placeholder="login"></p>
+				<p>Пароль<input type="password" name="password" placeholder="password"></p>
+				<p><input type="submit" value="Отправить"></p> 
+			</form>
+		</div>
+	-->
+	</body>
+</html>
